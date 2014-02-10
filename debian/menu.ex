26fda2922585ba172a4ccbe2ec335d09228f4387
Jean-Francois Dockes <jf@dockes.org>
2014-02-10 16:08:34 +0100

    added initial debian packaging

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(upmpdcli):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="upmpdcli" command="/usr/bin/upmpdcli"
---
+++ debian/upmpdcli.default.ex
@@ -0,0 +1,10 @@
+# Defaults for upmpdcli initscript
+# sourced by /etc/init.d/upmpdcli
+# installed at /etc/default/upmpdcli by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
